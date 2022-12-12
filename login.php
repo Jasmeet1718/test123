1a4b6a27dda38d1b64d96b8bea2a1618ec86f41f
Jasmeet1718 <65601510+Jasmeet1718@users.noreply.github.com>
2022-12-12 12:16:54 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -7,30 +7,7 @@
     <title>Login Page</title>
 </head>
 <body>
-    <style>
-        .main{
-            display:flex;
-            justify-content:center;
-            flex-direction:column;
-            align-items:center;
-        }
-        .form{
-            display:flex;
-            margin-top:50px;
-            justify-content:space-between;
-            flex-direction:column;
-            align-items:center;
-            height:300px;
-        }
-        input{
-            font-size:20px;
-            width:400px;
-            height:40px;
-        }
-        .submit{
-            font-size: 20px;
-        }
-    </style>
+    
     <div class="main">
         <h1>Login page</h1>
         <form method="post" class="form">
